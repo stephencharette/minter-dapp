43137f9aa756fcaf55b51e638a1c8078159ed335
Stephen <stephen.charette@outlook.com>
2022-06-22 21:35:08 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xECB73B353498b8a8754A55F3b6A1dD10E8ef76DB"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
